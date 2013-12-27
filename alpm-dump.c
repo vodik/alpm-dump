b8d57005123e05bc9b1bd53568b1ef0318b35576
Simon Gomizelj <simongmzlj@gmail.com>
2013-12-27 12:38:01 -0500

    support more entry types for -S and -Q

--- alpm-dump.c
+++ alpm-dump.c
@@ -19,8 +19,54 @@ enum table_entry_type {
     ENTRY_GROUPS,
     ENTRY_PROVIDES,
     ENTRY_DEPENDS,
+    ENTRY_REQUIRED,
+    ENTRY_OPTIONAL_FOR,
+    ENTRY_CONFLICTS,
+    ENTRY_REPLACES,
+    ENTRY_INSTALL_SIZE,
     ENTRY_PACKAGER,
+    ENTRY_BUILD_DATE,
+    ENTRY_INSTALL_DATE,
+    ENTRY_INSTALL_REASON,
+    ENTRY_INSTALL_SCRIPT,
+    ENTRY_VALIDATED,
     LAST_ENTRY
+};
+
+static const char *local_table[LAST_ENTRY] = {
+    [ENTRY_REPOSITORY]   = "Repository",
+    [ENTRY_NAME]         = "Name",
+    [ENTRY_VERSION]      = "Version",
+    [ENTRY_DESCRIPTION]  = "Description",
+    [ENTRY_ARCHITECTURE] = "Architecture",
+    [ENTRY_URL]          = "URL",
+    [ENTRY_LICENSES]     = "Licenses",
+    [ENTRY_GROUPS]       = "Groups",
+    [ENTRY_PROVIDES]     = "Provides",
+    [ENTRY_DEPENDS]      = "Depends On",
+    [ENTRY_REQUIRED]     = "Required By",
+    [ENTRY_INSTALL_SIZE] = "Install Size",
+    [ENTRY_OPTIONAL_FOR] = "Optional For",
+    [ENTRY_CONFLICTS]    = "Conflicts With",
+    [ENTRY_REPLACES]     = "Replaces",
+    [ENTRY_PACKAGER]     = "Packager"
+};
+
+static const char *sync_table[LAST_ENTRY] = {
+    [ENTRY_REPOSITORY]   = "Repository",
+    [ENTRY_NAME]         = "Name",
+    [ENTRY_VERSION]      = "Version",
+    [ENTRY_DESCRIPTION]  = "Description",
+    [ENTRY_ARCHITECTURE] = "Architecture",
+    [ENTRY_URL]          = "URL",
+    [ENTRY_LICENSES]     = "Licenses",
+    [ENTRY_GROUPS]       = "Groups",
+    [ENTRY_PROVIDES]     = "Provides",
+    [ENTRY_DEPENDS]      = "Depends On",
+    [ENTRY_INSTALL_SIZE] = "Install Size",
+    [ENTRY_CONFLICTS]    = "Conflicts With",
+    [ENTRY_REPLACES]     = "Replaces",
+    [ENTRY_PACKAGER]     = "Packager"
 };
 
 static size_t max_padding(const char *entries[static LAST_ENTRY])
@@ -74,6 +120,56 @@ static void print_deplist(alpm_list_t *list, unsigned short offset)
     }
 }
 
+/* {{{ */
+static double simple_pow(int base, int exp)
+{
+    double result = 1.0;
+    for(; exp > 0; exp--) {
+        result *= base;
+    }
+    return result;
+}
+
+static double humanize_size(off_t bytes, const char target_unit, int precision,
+                            const char **label)
+{
+    static const char *labels[] = {"B", "KiB", "MiB", "GiB",
+        "TiB", "PiB", "EiB", "ZiB", "YiB"};
+    static const int unitcount = sizeof(labels) / sizeof(labels[0]);
+
+    double val = (double)bytes;
+    int index;
+
+    for(index = 0; index < unitcount - 1; index++) {
+        if(target_unit != '\0' && labels[index][0] == target_unit) {
+            break;
+        } else if(target_unit == '\0' && val <= 2048.0 && val >= -2048.0) {
+            break;
+        }
+        val /= 1024.0;
+    }
+
+    if(label) {
+        *label = labels[index];
+    }
+
+    /* do not display negative zeroes */
+    if(precision >= 0 && val < 0.0 &&
+       val > (-0.5 / simple_pow(10, precision))) {
+        val = 0.0;
+    }
+
+    return val;
+}
+/* }}} */
+
+static void print_filesize(off_t size, unsigned short offset)
+{
+    const char *label;
+    double _size = humanize_size(size, '\0', 2, &label);
+    printf("%6.2f %s", _size, label);
+}
+
 static void print_table(const char *table[static LAST_ENTRY], alpm_pkg_t *pkg)
 {
     int i;
@@ -118,8 +214,33 @@ static void print_table(const char *table[static LAST_ENTRY], alpm_pkg_t *pkg)
         case ENTRY_DEPENDS:
             print_deplist(alpm_pkg_get_depends(pkg), width);
             break;
+        case ENTRY_REQUIRED:
+            print_list(alpm_pkg_compute_requiredby(pkg), width);
+            break;
+        case ENTRY_INSTALL_SIZE:
+            print_filesize(alpm_pkg_get_isize(pkg), width);
+            break;
+        case ENTRY_OPTIONAL_FOR:
+            print_list(alpm_pkg_compute_optionalfor(pkg), width);
+            break;
+        case ENTRY_CONFLICTS:
+            print_deplist(alpm_pkg_get_conflicts(pkg), width);
+            break;
+        case ENTRY_REPLACES:
+            print_deplist(alpm_pkg_get_replaces(pkg), width);
+            break;
         case ENTRY_PACKAGER:
             indentprint_r(alpm_pkg_get_packager(pkg), width, 0);
+            break;
+        case ENTRY_BUILD_DATE:
+            break;
+        case ENTRY_INSTALL_DATE:
+            break;
+        case ENTRY_INSTALL_REASON:
+            break;
+        case ENTRY_INSTALL_SCRIPT:
+            break;
+        case ENTRY_VALIDATED:
             break;
         default:
             break;
@@ -143,6 +264,7 @@ static void dump_db(alpm_db_t *db, const char *table[static LAST_ENTRY])
 int main(int argc, char *argv[])
 {
     int sync = 0;
+    const char **table = local_table;
     static const struct option opts[] = {
         { "sync",  no_argument, 0, 'S' },
         { "query", no_argument, 0, 'Q' },
@@ -156,6 +278,7 @@ int main(int argc, char *argv[])
 
         switch (opt) {
         case 'S':
+            table = sync_table;
             sync = 1;
             break;
         case 'Q':
@@ -166,20 +289,6 @@ int main(int argc, char *argv[])
 
     alpm_handle_t *handle = alpm_initialize("/", "/var/lib/pacman/", NULL);
     alpm_list_t *sync_dbs = sync_dbs = alpm_get_syncdbs(handle);
-
-    static const char *table[LAST_ENTRY] = {
-        [ENTRY_REPOSITORY]   = "Repository",
-        [ENTRY_NAME]         = "Name",
-        [ENTRY_VERSION]      = "Version",
-        [ENTRY_DESCRIPTION]  = "Description",
-        [ENTRY_ARCHITECTURE] = "Architecture",
-        [ENTRY_URL]          = "URL",
-        [ENTRY_LICENSES]     = "Licenses",
-        [ENTRY_GROUPS]       = "Groups",
-        [ENTRY_PROVIDES]     = "Provides",
-        [ENTRY_DEPENDS]      = "Depends On",
-        [ENTRY_PACKAGER]     = "Packager"
-    };
 
     if(sync) {
         const char *dbs[] = {
